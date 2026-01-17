e8474ce70644e30ca32598b63bf92e220b92030b
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2026-01-17 22:06:27 +0530

    Create 11. MUX_And_DFF1.v

---
+++ 2. Circuits/2. Sequential Logic/1. Latches and Flip-Flops/11. MUX_And_DFF1.v
@@ -0,0 +1,23 @@
+Consider the sequential circuit below:
+CKT:  https://hdlbits.01xz.net/mw/images/e/e5/Mt2015_muxdff.png
+
+Assume that you want to implement hierarchical Verilog code for this circuit, using three instantiations of a submodule that has a flip-flop and multiplexer in it. 
+  Write a Verilog module (containing one flip-flop and multiplexer) named top_module for this submodule.
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+module top_module (
+	input clk,
+	input L,
+	input r_in,
+	input q_in,
+	output reg Q);
+
+     always @(posedge clk) begin
+     if(L)
+        Q <= r_in;
+     else
+        Q <= q_in;
+  end
+    
+endmodule
