2373ecfb337131de0621c0b53a415bdbaf2ef6da
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2026-01-27 13:22:20 +0530

    Add synchronous reset to Moore state machine
    Implemented a Moore state machine with synchronous reset logic.

---
+++ 2. Circuits/2. Sequential Logic/5. FInite State Machines/02. Simple-FSM1(SynchronousReset).v
@@ -0,0 +1,37 @@
+This is a Moore state machine with two states, one input, and one output. Implement this state machine. Notice that the reset state is B.
+
+This exercise is the same as fsm1, but using synchronous reset.
+
+STATE DIAGRAM:  https://hdlbits.01xz.net/mw/images/8/82/Fsm1s.png
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+module top_module(clk, reset, in, out);
+    input clk;
+    input reset;    // Synchronous reset to state B
+    input in;
+    output out;//  
+    reg out;
+
+    // Fill in state name declarations
+
+    reg present_state, next_state;
+
+    always @(posedge clk) begin
+        if (reset) begin  
+            // Fill in reset logic
+        end else begin
+            case (present_state)
+                // Fill in state transition logic
+            endcase
+
+            // State flip-flops
+            present_state = next_state;   
+
+            case (present_state)
+                // Fill in output logic
+            endcase
+        end
+    end
+
+endmodule
