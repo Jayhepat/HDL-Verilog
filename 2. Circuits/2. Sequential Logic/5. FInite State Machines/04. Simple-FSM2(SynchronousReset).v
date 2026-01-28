1d2eb7b7a84c98b2e7e6af02192f37f66495f2c2
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2026-01-28 08:16:03 +0530

    Add Simple FSM with synchronous reset functionality
    Implement a Moore state machine with synchronous reset.

---
+++ 2. Circuits/2. Sequential Logic/5. FInite State Machines/04. Simple-FSM2(SynchronousReset).v
@@ -0,0 +1,34 @@
+This is a Moore state machine with two states, two inputs, and one output. Implement this state machine.
+
+State Diagram :   https://hdlbits.01xz.net/mw/images/5/5d/Fsmjks.png
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+module top_module(
+    input clk,
+    input reset,    // Synchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+        case(state)
+            OFF : next_state = (j == 1) ? ON : OFF;
+            ON : next_state = ( k == 1) ? OFF : ON;
+        endcase
+    end
+
+    always @(posedge clk) begin
+        // State flip-flops with synchronous reset
+        if (reset) state <= OFF;
+        else state <= next_state;
+    end
+    
+    
+    // Output logic
+    assign out = (state == ON);
+   
+endmodule
