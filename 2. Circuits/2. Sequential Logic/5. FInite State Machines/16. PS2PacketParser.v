258672bb21718ad15320fcfe0982e9b73f184a39
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2026-01-31 10:36:50 +0530

    Create 16. PS2PacketParser.v

---
+++ 2. Circuits/2. Sequential Logic/5. FInite State Machines/16. PS2PacketParser.v
@@ -0,0 +1,51 @@
+The PS/2 mouse protocol sends messages that are three bytes long. However, within a continuous byte stream, it's not obvious where messages start and end.
+The only indication is that the first byte of each three byte message always has bit[3]=1 (but bit[3] of the other two bytes may be 1 or 0 depending on data).
+
+We want a finite state machine that will search for message boundaries when given an input byte stream. The algorithm we'll use is to discard bytes until we see one with bit[3]=1.
+We then assume that this is byte 1 of a message, and signal the receipt of a message once all 3 bytes have been received (done).
+
+The FSM should signal done in the cycle immediately after the third byte of each message was successfully received.
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+module top_module(
+    input clk,
+    input [7:0] in,
+    input reset,    // Synchronous reset
+    output done); //
+    
+     localparam [1:0] BYTE1 = 2'b00,
+    				 BYTE2 = 2'b01,
+    				 BYTE3 = 2'b10,
+    				 DONE  = 2'b11;
+
+    reg [1:0] state, next;
+    reg [23:0] data;
+
+    // State transition logic (combinational)
+    always @(*) begin
+    	case(state)
+    		BYTE1 : next = (in[3]) ? BYTE2 : BYTE1;
+    		BYTE2 : next = BYTE3;
+    		BYTE3 : next = DONE;
+    		DONE  : next = (in[3]) ? BYTE2 : BYTE1;
+    	endcase
+    end
+
+    // State flip-flops (sequential)
+    always @(posedge clk) begin
+    	if(reset) state <= BYTE1;
+    	else state <= next;
+    end
+ 
+    // New: Datapath to store incoming bytes.
+    always @(posedge clk) begin
+    	if (reset) data <= 24'b0;
+    	else data <= {data[15:8], data[7:0], in};
+    end
+    
+    // Output logic
+    assign done = (state == DONE);
+    assign out_bytes = (done) ? data : 24'b0;
+
+endmodule
