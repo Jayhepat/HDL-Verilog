a1b6e7de006695965ab2831c938b2e7c4259368a
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2026-02-01 11:49:26 +0530

    Create 18. SerialReceiverAndDatapath.v

---
+++ 2. Circuits/2. Sequential Logic/5. FInite State Machines/18. SerialReceiverAndDatapath.v
@@ -0,0 +1,80 @@
+Now that you have a finite state machine that can identify when bytes are correctly received in a serial bitstream, add a datapath that will output the correctly-received data byte.
+out_byte needs to be valid when done is 1, and is don't-care otherwise.
+
+Note that the serial protocol sends the least significant bit first.
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
+    output [7:0] out_byte,
+    output done
+); //
+
+     // Use FSM from Fsm_serial
+    localparam [2:0] IDLE 	 = 3'b000,
+					 START 	 = 3'b001,
+					 RECEIVE = 3'b010,
+					 WAIT	 = 3'b011,
+					 STOP    = 3'b100;
+
+	reg [2:0] state, next;
+	reg [3:0] i;
+	reg [7:0] out;
+
+	always @(*) begin
+		case(state)
+			IDLE  : next = (in) ? IDLE : START;
+			START : next = RECEIVE;
+			RECEIVE : begin
+				if (i == 8) begin
+					if (in) next = STOP;
+					else next = WAIT;
+				end 
+				else next = RECEIVE;			
+			end
+			WAIT : next = (in) ? IDLE : WAIT;
+			STOP : next = (in) ? IDLE : START;
+		endcase
+	end
+
+	always @(posedge clk) begin
+		if(reset) state <= IDLE;
+		else state <= next;
+	end
+
+	always @(posedge clk) begin
+		if (reset) begin
+			done <= 0;
+			i <= 0;
+		end
+		else begin
+			case(next) 
+				RECEIVE : begin
+					done <= 0;
+					i = i + 4'h1;
+				end
+				STOP : begin
+					done <= 1;
+					i <= 0;
+				end
+				default : begin
+					done <= 0;
+					i <= 0;
+				end
+			endcase
+		end
+	end
+
+    // New: Datapath to latch input bits.
+    always @(posedge clk) begin
+    	if (reset) out <= 0;
+    	else if (next == RECEIVE)
+    		out[i] <= in;
+    end
+
+    assign out_byte = (done) ? out : 8'b0;
+
+endmodule
