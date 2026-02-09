f066c7271db8608e2041136994c9895ae804ad33
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2026-02-09 19:07:32 +0530

    Create 02. 4-BitShiftRegisterAndDownCounter.v

---
+++ 2. Circuits/3. Building Larger Circuits/02. 4-BitShiftRegisterAndDownCounter.v
@@ -0,0 +1,29 @@
+This is the first component in a series of five exercises that builds a complex counter out of several smaller circuits. See the final exercise for the overall design.
+
+Build a four-bit shift register that also acts as a down counter. Data is shifted in most-significant-bit first when shift_ena is 1. The number currently in the shift register is decremented when count_ena is 1.
+Since the full system doesn't ever use shift_ena and count_ena together, it does not matter what your circuit does if both control inputs are 1 (This mainly means that it doesn't matter which case gets higher priority).
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+    
+    reg [3:0] shift_temp;
+
+	always @(posedge clk) begin
+		if (shift_ena) begin
+			shift_temp <= {shift_temp[2:0], data};
+		end
+		else if (count_ena) begin
+			shift_temp <= shift_temp - 1'b1;
+		end 
+	end
+
+	assign q = shift_temp;
+
+endmodule
+
