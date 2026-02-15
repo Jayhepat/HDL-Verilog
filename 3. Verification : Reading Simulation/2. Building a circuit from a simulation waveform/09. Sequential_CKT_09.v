baaaf351092d81d6fa87c0d949f0869ff996db89
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2026-02-15 20:53:51 +0530

    Create 09. Sequential_CKT_09.v

---
+++ 3. Verification : Reading Simulation/2. Building a circuit from a simulation waveform/09. Sequential_CKT_09.v
@@ -0,0 +1,18 @@
+This is a sequential circuit. Read the simulation waveforms to determine what the circuit does, then implement it.
+
+----CORRECT CODE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+module top_module (
+    input clk,
+    input a,
+    output [3:0] q );
+    
+    always @(posedge clk) begin
+        if (a) q <= 4;
+        else begin
+            if (q < 6) q <= q + 1;
+            else q <= 0;
+        end
+    end
+
+endmodule
